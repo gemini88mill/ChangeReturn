1363a25608d0f229ba823f660ea8336971fbb4df
Raphael <gemini88mill@gmail.com>
2014-12-30 18:55:53 -0500

    first Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+/**
+ * Change Return program:
+ *
+ * The user enters a cost and then the amount of money given.
+ * The program will figure out the change and the number of
+ * quarters, dimes, nickels, pennies needed for the change.
+ *
+ * @author Raphael Miller
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("hello world ");
+    }
+}
