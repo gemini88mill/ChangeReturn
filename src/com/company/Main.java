96ba57370c083c4b90af3937f098a1ed05f1a9ad
Raphael <gemini88mill@gmail.com>
2014-12-31 10:04:02 -0500

    added comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,43 +14,53 @@ import java.util.Arrays;
  */
 public class Main {
 
+    //variables in constructor
     private double amountTendered;
     private double productVal;
 
-
+    //all other variables without constructor
     private double changeVal;
     private int[] change = new int[8];
+
+    //finals
     private final String[] changeTitle = {"Twenty(s)", "Ten(s)  ", "Five(s)  ", "One(s)  ", "Quarter(s)", "Dime(s)  ", "Nickel(s)", "Penny(s)"};
-
-    private final double TWENTY_VAL = 20.00;
-    private final double TEN_VAL = 10.00;
-    private final double FIVE_VAL = 5.00;
-    private final double DOLLAR_VAL = 1.00;
-
-    private final double QUARTER_VAL = 0.25;
-    private final double DIME_VAL = 0.10;
-    private final double NICKEL_VAL = 0.05;
-    private final double PENNY_VAL = 0.01;
-
     private final double[] TENDER_VAL = {20.00, 10.00, 5.00, 1.00, 0.25, 0.10, 0.05, 0.01};
 
 
+    /**
+     * main method
+     *
+     * starts program, executes two values within the constructor and proceeds
+     * to calculate the amount of change needed to make correct change.
+     * @param args
+     */
     public static void main(String[] args) {
-        Main main = new Main(51.00, 47.06);
+        Main main = new Main(51.00, 47.89);
 
         main.figureChange();
+       // main.roundChange();
         System.out.println("Change Amount: " + main.getChangeVal());
 
         main.giveChange();
         main.changePrintOut();
     }
 
+
+    /**
+     * simple change print out
+     */
     private void changePrintOut() {
         for (int x = 0; x < getChange().length; x++){
             System.out.println(changeTitle[x] + "\t\t\t " + getChange()[x]);
         }
     }
 
+    /**
+     * giveChange()
+     *
+     * change calculated and counts the tender amount needed to give back
+     * the correct amount of change
+     */
     private void giveChange() {
         double change = getChangeVal();
         double holder = 0;
@@ -74,6 +84,9 @@ public class Main {
 
     }
 
+    /**
+     * calculates change needed from perams
+     */
     private void figureChange() {
         setChangeVal(getAmountTendered() - getProductVal());
 
